e53192246ef5cc73cdfbabfda946193f4c20c7e2
B2014728 <lethanhbach2002@gmail.com>
2024-02-21 17:17:22 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 
@@ -12,5 +13,6 @@ app.get("/", (req,res) =>{
         message: "Welcome to contact book application."
     });
 });
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
